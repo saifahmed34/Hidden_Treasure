342da17680554274a84ab26f07f6927b1e5e3f5c
saifahmed34 <saifdragonball5@gmail.com>
2025-09-01 08:20:08 +0300

    edited in levels code

--- hidden Treasure/Assets/Scripts/MainMap/LevelS/LevelStart.cs
+++ hidden Treasure/Assets/Scripts/MainMap/LevelS/LevelStart.cs
@@ -1,31 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class LevelStart : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     int Completed_Level = 0;
-    void Start()
-    {
+    public GameObject player;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Level")
+        if (collision.gameObject.tag == "Level1")
         {
-            LevelNumber levelNumber = new LevelNumber();
-            if (levelNumber != null && levelNumber.LevelIndex == Completed_Level + 1)
-            {
-                SceneManager.LoadScene(levelNumber.LevelName);
-                Completed_Level++;
-            }
+            SceneManager.LoadScene("Level1");
+
+            Completed_Level++;
         }
-
-
+        else if (collision.gameObject.tag == "Level2" && Completed_Level == 1)
+        {
+            SceneManager.LoadScene("Level2");
+            Completed_Level++;
+        }
+        else if (collision.gameObject.tag == "Level3" && Completed_Level == 2)
+        {
+            SceneManager.LoadScene("Level3");
+            Completed_Level++;
+        }
+        else if (collision.gameObject.tag == "lastlevel" && Completed_Level == 3)
+        {
+            SceneManager.LoadScene("lastlevel");
+        }
     }
 }
